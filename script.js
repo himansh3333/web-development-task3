79f90dfa5bc6604c1f2816fc6384f3aa0e69af1e
himansh3333 <himanshbhagi15@gmail.com>
2026-07-11 21:41:20 +0530

    script.js task 3

--- script.js
+++ script.js
@@ -245,3 +245,253 @@ modal.style.display="none";
 });
 
 }
+// Weather App
+const cityInput = document.getElementById("cityInput");
+
+if(cityInput){
+    const lastCity = localStorage.getItem("lastCity");
+
+    if(lastCity){
+        cityInput.value = lastCity;
+    }
+}
+const searchWeather = document.getElementById("searchWeather");
+
+if(searchWeather){
+
+searchWeather.addEventListener("click", getWeather);
+
+}
+
+async function getWeather(){
+
+const city=document.getElementById("cityInput").value.trim();
+
+const result=document.getElementById("weatherResult");
+
+if(city===""){
+
+result.innerHTML="<p>Please enter a city name.</p>";
+
+return;
+
+}
+
+const apiKey="04ade286a0d77521a35748ab2e6f3cbc";
+
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+try{
+
+const response=await fetch(url);
+
+const data=await response.json();
+
+console.log(data);
+if(data.cod != 200){
+    result.innerHTML = "<p>City not found.</p>";
+    return;
+}
+
+result.innerHTML = `
+<h2>${data.name}</h2>
+<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+<p>🌡️ Temperature: ${data.main.temp} °C</p>
+<p>💧 Humidity: ${data.main.humidity}%</p>
+<p>💨 Wind Speed: ${data.wind.speed} m/s</p>
+<p>☁️ ${data.weather[0].description}</p>
+`;
+localStorage.setItem("lastCity", city);
+}
+catch(error){
+
+result.innerHTML="<p>Something went wrong.</p>";
+
+}
+
+}
+// Todo App
+
+function getTasks() {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+
+function saveTasks(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+const taskInput = document.getElementById("taskInput");
+const addTask = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
+
+function createTask(taskText, completed = false) {
+
+    const li = document.createElement("li");
+
+    if (completed) {
+        li.classList.add("completed");
+    }
+
+    li.innerHTML = `
+<span>${taskText}</span>
+
+<div>
+<button class="completeBtn">✅</button>
+<button class="editBtn">✏️</button>
+<button class="deleteBtn">🗑️</button>
+</div>
+`;
+
+    taskList.appendChild(li);
+
+    li.querySelector(".completeBtn").addEventListener("click", () => {
+
+    li.classList.toggle("completed");
+
+    const tasks = getTasks();
+
+    const taskIndex = tasks.findIndex(t => t.text === taskText);
+
+    if(taskIndex !== -1){
+
+        tasks[taskIndex].completed = li.classList.contains("completed");
+
+        saveTasks(tasks);
+
+    }
+
+});
+
+    li.querySelector(".deleteBtn").addEventListener("click", () => {
+
+    const tasks = getTasks();
+
+    const updatedTasks = tasks.filter(t => t.text !== taskText);
+
+    saveTasks(updatedTasks);
+
+    li.remove();
+
+});
+li.querySelector(".editBtn").addEventListener("click", () => {
+
+    const span = li.querySelector("span");
+
+    const oldTask = span.innerText;
+
+    const updatedTask = prompt("Edit Task", oldTask);
+
+    if (updatedTask && updatedTask.trim() !== "") {
+
+        span.innerText = updatedTask.trim();
+
+        const tasks = getTasks();
+
+        const taskIndex = tasks.findIndex(t => t.text === oldTask);
+
+        if(taskIndex !== -1){
+
+            tasks[taskIndex].text = updatedTask.trim();
+
+            saveTasks(tasks);
+
+        }
+
+        taskText = updatedTask.trim();
+
+    }
+
+});
+
+}
+
+if (addTask) {
+
+    addTask.addEventListener("click", () => {
+
+        const task = taskInput.value.trim();
+
+        if (task === "") {
+
+            alert("Please enter a task.");
+
+            return;
+
+        }
+
+        createTask(task);
+
+        const tasks = getTasks();
+
+        tasks.push({
+            text: task,
+            completed: false
+        });
+
+        saveTasks(tasks);
+
+        taskInput.value = "";
+
+    });
+
+}
+// Load Saved Tasks
+
+window.addEventListener("load", () => {
+
+    const tasks = getTasks();
+
+    tasks.forEach(task => {
+
+        createTask(task.text, task.completed);
+
+    });
+
+});
+// Filters
+
+const allBtn = document.getElementById("allBtn");
+const activeBtn = document.getElementById("activeBtn");
+const completedBtn = document.getElementById("completedBtn");
+
+function filterTasks(type){
+
+const tasks = document.querySelectorAll("#taskList li");
+
+tasks.forEach(task=>{
+
+if(type==="all"){
+
+task.style.display="flex";
+
+}
+
+else if(type==="active"){
+
+task.style.display = task.classList.contains("completed")
+? "none"
+: "flex";
+
+}
+
+else if(type==="completed"){
+
+task.style.display = task.classList.contains("completed")
+? "flex"
+: "none";
+
+}
+
+});
+
+}
+
+if(allBtn){
+
+allBtn.addEventListener("click",()=>filterTasks("all"));
+
+activeBtn.addEventListener("click",()=>filterTasks("active"));
+
+completedBtn.addEventListener("click",()=>filterTasks("completed"));
+
+                         }
